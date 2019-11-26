6756c5f79b53b67f659f56e969b380c99666dd4d
Huschdi111 <lukatoni_mueller@hotmail.com>
2019-11-26 23:34:09 +0100

    Bug Fix: Fixed Export Problem, Paricipants are now in a json Array

--- Neosmartpen.Net/Neosmartpen/Net/Export_Import/JsonFormatter.cs
+++ Neosmartpen.Net/Neosmartpen/Net/Export_Import/JsonFormatter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Neosmartpen.Net.Metadata.Model;
 using Newtonsoft.Json;
 
@@ -9,14 +10,12 @@ namespace Neosmartpen.Net.Neosmartpen.Net.Export_Import
         ///Serializes the given ParticipantsMap into a json object
         ///</summary>
         public static String Format(System.Collections.Generic.Dictionary<String, Participant> ParticipantsMap) {
-            String dataJson = "";
+            List<CompressedParticipant> comParticipants = new List<CompressedParticipant>();
             foreach (String k in ParticipantsMap.Keys)
             {
-               String json = JsonConvert.SerializeObject(new CompressedParticipant(ParticipantsMap[k]), Formatting.Indented);
-               Console.WriteLine(json);
-               dataJson += json;
+               comParticipants.Add(new CompressedParticipant(ParticipantsMap[k]));
             }
-            return dataJson;
+            return JsonConvert.SerializeObject(comParticipants, Formatting.Indented); ;
         }
     }
 }
